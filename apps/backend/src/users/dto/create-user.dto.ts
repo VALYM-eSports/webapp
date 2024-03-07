f3a093bc6b206e28707dcef2c0e3e6dc9c24c9bb
Yoan Ancelly <yoan.a@sonup.fr>
2024-03-07 12:38:06 +0100

    feat(WEB-17): add create user

---
+++ apps/backend/src/users/dto/create-user.dto.ts
@@ -0,0 +1,8 @@
+import { IsEmail, IsNotEmpty } from 'class-validator';
+
+export class CreateUserDto {
+  @IsEmail()
+  readonly email: string;
+  @IsNotEmpty()
+  readonly name: string;
+}
---
+++ apps/backend/src/users/users.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
+
+@Injectable()
+export class UsersService {
+  constructor(private readonly prismaService: PrismaService) {}
+  create(createUserDto: CreateUserDto) {
+    return this.prismaService.user.create({ data: createUserDto });
+  }
+
+  findAll() {
+    return `This action returns all users`;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} user`;
+  }
+
+  update(id: number, updateUserDto: UpdateUserDto) {
+    return `This action updates a #${id} user`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} user`;
+  }
+}
