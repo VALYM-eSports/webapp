b071f98225cf8db370aebc14087547725c5ac0f6
Yoan Ancelly <yoan.a@sonup.fr>
2024-03-07 19:46:31 +0100

    chore(WEB-17): add user entity

--- apps/backend/src/users/entities/user.entity.ts
+++ apps/backend/src/users/entities/user.entity.ts
@@ -1,1 +1,22 @@
-export class User {}
+import { ApiProperty } from '@nestjs/swagger';
+import { User } from '@prisma/client';
+
+export class UserEntity implements User {
+  @ApiProperty()
+  id: number;
+
+  @ApiProperty()
+  email: string;
+
+  @ApiProperty()
+  name: string;
+
+  @ApiProperty()
+  isActive: boolean;
+
+  @ApiProperty()
+  createdAt: Date;
+
+  @ApiProperty()
+  updatedAt: Date;
+}
