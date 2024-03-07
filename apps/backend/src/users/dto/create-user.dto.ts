08df2f935ed318a79abc7438cadf3c2d3db0837b
Yoan Ancelly <yoan.a@sonup.fr>
2024-03-07 15:05:10 +0100

    chore(WEB-17): add ApiProperty create-user

--- apps/backend/src/users/dto/create-user.dto.ts
+++ apps/backend/src/users/dto/create-user.dto.ts
@@ -1,8 +1,11 @@
 import { IsEmail, IsNotEmpty } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateUserDto {
+  @ApiProperty()
   @IsEmail()
   readonly email: string;
+  @ApiProperty()
   @IsNotEmpty()
   readonly name: string;
 }
