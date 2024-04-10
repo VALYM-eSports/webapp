ea93b161fd5d6b79adbadd7d1242db0e316759b2
Yoan Ancelly <git@nizrod.ovh>
2024-04-10 16:50:47 +0200

    lint(WEB-36): apply biome safe suggestions

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,20 +1,20 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { NestFactory } from "@nestjs/core";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
+import { AppModule } from "./app.module";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+	const app = await NestFactory.create(AppModule);
 
-  const config = new DocumentBuilder()
-    .setTitle('VALYM eSports')
-    .setDescription('The VALYM eSports API')
-    .setVersion('0.1')
-    .build();
+	const config = new DocumentBuilder()
+		.setTitle("VALYM eSports")
+		.setDescription("The VALYM eSports API")
+		.setVersion("0.1")
+		.build();
 
-  const document = SwaggerModule.createDocument(app, config);
+	const document = SwaggerModule.createDocument(app, config);
 
-  SwaggerModule.setup('api', app, document);
+	SwaggerModule.setup("api", app, document);
 
-  await app.listen(3001);
+	await app.listen(3001);
 }
 bootstrap();
